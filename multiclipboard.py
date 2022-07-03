454323fef6ed6239d693fdcb5196ed529a3b61f0
yashxa <yawasthi6113@gmail.com>
2022-07-03 21:40:40 +0530

    First Commit 3 Jun 2022

---
+++ multiclipboard.py
@@ -0,0 +1,51 @@
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+#save_data("clipboard.json",{"key": "value"})
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+
+#clipboard.copy("abc")
+#data = clipboard.paste()
+#print(data)
+
+#print(sys.argv)
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("Data Saved")
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key does not exist.")
+
+    elif command == "list":
+        print(data)
+    else:
+        print("ERROR: Command Not Found.\nERROR: Invalid Command.")
+
+else:
+    print("ERROR: Enter one command at once.")
